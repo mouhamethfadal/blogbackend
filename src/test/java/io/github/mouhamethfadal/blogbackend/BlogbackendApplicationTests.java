f36a847d40fd44b86afb3e31c48056d495d5dfe5
Mouhameth Fadal Maraby Aidara <mouhamethfadalaidara@gmail.com>
2024-12-23 09:43:38 +0000

    :tada::construction: Set up blog backend

---
+++ src/test/java/io/github/mouhamethfadal/blogbackend/BlogbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mouhamethfadal.blogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
