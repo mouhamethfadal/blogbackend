e6bd94d6ab35f47eccd255c0180cb75a650281a5
Mouhameth Fadal Maraby Aidara <mouhamethfadalaidara@gmail.com>
2025-01-24 08:08:10 +0000

    :sparkles: feat(user): add user model and repository

---
+++ src/main/java/io/github/mouhamethfadal/blogbackend/model/User.java
@@ -0,0 +1,26 @@
+package io.github.mouhamethfadal.blogbackend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collection = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    private String id;
+    private String username;
+    private String email;
+    private String password;
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+}
---
+++ src/main/java/io/github/mouhamethfadal/blogbackend/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package io.github.mouhamethfadal.blogbackend.repository;
+
+import io.github.mouhamethfadal.blogbackend.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
