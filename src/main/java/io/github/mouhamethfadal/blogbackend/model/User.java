a0aec9ee955d7cad01c9aac46add47f3702555cf
Mouhameth Fadal Maraby Aidara <mouhamethfadalaidara@gmail.com>
2025-02-10 09:04:26 +0000

    🦺 feat(validation): add validation to users

--- src/main/java/io/github/mouhamethfadal/blogbackend/model/User.java
+++ src/main/java/io/github/mouhamethfadal/blogbackend/model/User.java
@@ -1,5 +1,7 @@
 package io.github.mouhamethfadal.blogbackend.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,9 +20,16 @@ import java.util.List;
 public class User {
     @Id
     private String id;
+    @NotBlank(message = "Username is required")
     private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
+
     @Builder.Default
     private List<String> roles = new ArrayList<>();
 }
