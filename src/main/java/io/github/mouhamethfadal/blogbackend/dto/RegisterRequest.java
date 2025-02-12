c63670717b1d9cfb25cd3d726e69b10840a8ce27
Mouhameth Fadal Maraby Aidara <mouhamethfadalaidara@gmail.com>
2025-02-12 08:23:59 +0000

    :sparkles: feat(authentication): set up authentication DTOs

---
+++ src/main/java/io/github/mouhamethfadal/blogbackend/dto/RegisterRequest.java
@@ -0,0 +1,19 @@
+package io.github.mouhamethfadal.blogbackend.dto;
+
+import io.github.mouhamethfadal.blogbackend.validation.annotation.StrongPassword;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    @NotBlank(message = "Username should not be blank")
+    private String username;
+
+    @NotBlank(message = "Email should not be blank")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @StrongPassword(message = "Password must be at least 8 characters long and contain uppercase, lowercase, number, special character")
+    private String password;
+}
