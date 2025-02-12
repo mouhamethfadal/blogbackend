115eab06192b3b700e5f93aadb5b95c96a6f22d0
Mouhameth Fadal Maraby Aidara <mouhamethfadalaidara@gmail.com>
2025-02-12 08:23:59 +0000

    :sparkles: feat(authentication): set up authentication DTOs

---
+++ src/main/java/io/github/mouhamethfadal/blogbackend/entity/Role.java
@@ -0,0 +1,6 @@
+package io.github.mouhamethfadal.blogbackend.entity;
+
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_USER
+}
---
+++ src/main/java/io/github/mouhamethfadal/blogbackend/validation/annotation/StrongPassword.java
@@ -0,0 +1,19 @@
+package io.github.mouhamethfadal.blogbackend.validation.annotation;
+
+import io.github.mouhamethfadal.blogbackend.validation.validator.StrongPasswordValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = StrongPasswordValidator.class)
+public @interface StrongPassword {
+    String message () default "Invalid password";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
