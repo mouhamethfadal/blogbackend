023d79ac9f8c2f4bb2ff686b9759d052746ed8e6
Mouhameth Fadal Maraby Aidara <mouhamethfadalaidara@gmail.com>
2025-04-23 13:40:45 +0000

    🦺 feat: Adds validation for user registration
    Implements validation for user registration requests.

--- src/main/java/io/github/mouhamethfadal/blogbackend/dtos/RegisterRequest.java
+++ src/main/java/io/github/mouhamethfadal/blogbackend/dtos/RegisterRequest.java
@@ -14,6 +14,6 @@ public class RegisterRequest {
     @Email(message = "Email should be valid")
     private String email;
 
-    @StrongPassword(message = "Password must have 8+ with at least: 1 uppercase, 1 lowercase, 1 number, 1 special char")
+    @StrongPassword(message = "Password must have 8+ characters with at least: 1 uppercase, 1 lowercase, 1 number, 1 special char")
     private String password;
 }
